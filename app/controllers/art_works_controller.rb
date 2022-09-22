d8d47c4d7de40ae7e0eeacc70578d9ca21bd366c
magnetron-2000 <danylokulba@gmail.com>
2022-09-22 23:02:24 +0300

    allow author to update and delete only his ArtWork

--- app/controllers/art_works_controller.rb
+++ app/controllers/art_works_controller.rb
@@ -1,4 +1,6 @@
 class ArtWorksController < ApplicationController
+  before_action :find_art_work, only: [:edit, :update, :destroy, :let_change]
+  before_action :let_change, only: [:edit, :update, :destroy]
 
   def show
     @art_works = ArtWork.all.where :user_id => params[:id].to_i
@@ -22,11 +24,9 @@ class ArtWorksController < ApplicationController
   end
 
   def edit
-    @art_work = ArtWork.find(params[:id])
   end
 
   def update
-    @art_work = ArtWork.find(params[:id])
     if @art_work.update(art_params)
       redirect_to "/art_works/#{current_user.id}"
     else
@@ -35,13 +35,23 @@ class ArtWorksController < ApplicationController
   end
 
   def destroy
-    @art_work = ArtWork.find(params[:id])
     @art_work.destroy
     redirect_to "/art_works/#{current_user.id}"
+  end
+
+  def let_change # Allow author to update and delete only his ArtWork
+    if current_user.id != @art_work.user_id
+      flash.alert = "You can not change someone else's work"
+      redirect_to "/users"
+    end
   end
 
   private
   def art_params
     params.require(:art_work).permit(:title, :attachment, :resize_to_fit)
+  end
+
+  def find_art_work
+    @art_work = ArtWork.find(params[:id])
   end
 end
